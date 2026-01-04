502b68379e1fcd235f6ae0024cd83406fb2e2bab
DGStarleon <dgstarleon.projectid@gmail.com>
2026-01-05 00:17:07 +0700

    Delete config/koneksi.php

--- config/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$pdo = new PDO(
-    "mysql:host=localhost;dbname=nayla_laundry_uponhotred;charset=utf8mb4",
-    "nayla_laundry_uponhotred", "6508bb8dd97251f0f9c682d3d459e577763ea466", 
-    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
-);
-
-?>
