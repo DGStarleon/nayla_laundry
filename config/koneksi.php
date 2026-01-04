b16800eb01e5de8aba2ee47f0dcc1c8950aa6bf4
DGStarleon <dgstarleon.projectid@gmail.com>
2026-01-04 22:50:24 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,7 +1,8 @@
 <?php
 $pdo = new PDO(
-    "mysql:host=localhost;dbname=nayla_laundry;charset=utf8mb4",
-    "root", "", 
+    "mysql:host=localhost;dbname=nayla_laundry_uponhotred;charset=utf8mb4",
+    "nayla_laundry_uponhotred", "6508bb8dd97251f0f9c682d3d459e577763ea466", 
     [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
 );
+
 ?>
